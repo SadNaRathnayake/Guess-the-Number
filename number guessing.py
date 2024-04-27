a76565e80010e0c15a370768d40c8fa3e8cd1dec
Dilshani Rathnayake <149291402+SadNaRathnayake@users.noreply.github.com>
2024-04-27 23:59:55 +0530

    This fixes #1

--- number guessing.py
+++ number guessing.py
@@ -1,22 +1,32 @@
 import random
 
+while True:
+    try:
+        start = int(input("Enter the start: "))
+        end = int(input("Enter the end: "))
 
-start=int(input("enter the start :\n"))
-end=int(input("enter the end :\n "))
-              
+        # Check if both start and end are integers
+        if isinstance(start, int) and isinstance(end, int):
+            if start >= end:
+                print("End must be greater than start.")
+            else:
+                break
+        else:
+            print("Both start and end must be integers.")
+    except ValueError:
+        print("Both start and end must be integers.")
 
-ran_number=random.randint(start,end)
+ran_number = random.randint(start, end)
 
-user_integer=0
-
-while user_integer != ran_number:
-
-    user_integer=int(input("enter the number :"))
-    
-    if user_integer < ran_number :
-        print("too high")
-
-    elif user_integer > ran_number:
-        print("Too low")
-
-print("youuuu're correct !")
+while True:
+    try:
+        user_integer = int(input("Enter your guess: "))
+        if user_integer < ran_number:
+            print("Too low")
+        elif user_integer > ran_number:
+            print("Too high")
+        else:
+            print("You're correct!")
+            break
+    except ValueError:
+        print("Please enter a valid integer.")
